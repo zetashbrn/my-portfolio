4b1dfbf7f8c09d26b5b4aa0a5fa4f456cbe9cb29
Zeta Shabrina Arifa <zeta.arifa@binus.ac.id>
2023-09-25 11:21:14 +0700

    create component

--- my-portofolio/src/App.js
+++ my-portofolio/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Sidebar from './components/sidebar/Sidebar';
+import About from './components/about/About';
+import Home from './components/home/Home';
+import Project from './components/project/Project';
+import Skills from './components/skills/Skills';
+import Certificate from './components/certificate/Certificate';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Sidebar />
+      <main className='main'>
+        <Home />
+        <About />
+        <Skills />
+        <Project />
+        <Certificate />
+      </main>
+    </>
+  )
 }
 
 export default App;
---
+++ my-portofolio/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
